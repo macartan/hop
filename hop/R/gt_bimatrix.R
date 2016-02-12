101192035c4ce8ea2956048fc6ff99b59c545eb8
macartan <macartan@gmail.com>
2016-02-12 12:26:07 -0500

    initial test of package creation
    includes renaming and cleaning up basic functions

---
+++ hop/R/gt_bimatrix.R
@@ -0,0 +1,136 @@
+
+#' Payoff Matrix
+#'
+#' This function helps you plot a payoff matrix and identify pure strategy Nash equilibria
+#' @param X Payoff matrix. Defaults to matching pennies.
+#' @keywords 
+#' @export
+#' @examples
+#' gt_bimatrix()
+#' 
+
+
+# plots bimatrix and shades pure strat NE whereX and Y are 2x2 payoff matrices, and c is scale parameter for how big the text should be, 
+# plotstyle can be "s"or "m" where "s" uses square plotting and "m" uses maximal area plotting
+# srt1,2 Turns the writing on the player labels
+# u scale parameter for utility labels
+# matrixfill=gray(.9), numcol="black", gameborder=T, bordercol="white", borderdim = NULL, 
+# main : plot title
+# tscale : scaling parameter to control all labels
+# offset1=c(.8,.8), offset2=c(.2,.2), arrowdist=.15, radius=.2, space=max(arrowdist, radius+.075), 
+# arrow1=TRUE, arrow2=TRUE, 
+# arrow1col=gray(.4), arrow2col=gray(.4), 
+# width1=3, width2=3, 
+# nash=T, nashcol=gray(.6), nashborder="black", 
+# labels1=NULL, labels2=NULL, labelsize=NULL, p1="Player 1", p2="Player 2", playersize=NULL, labelsdim = NULL, pdim =NULL){ 
+
+gt_bimatrix <- function(
+            X = matrix(c(0,1,1,0),2),              # Payoff matrix
+            Y=t(X), 
+						P1="Player 1", 
+						P2="Player 2", 
+						labels1=NULL, 
+						labels2=labels1, 
+						labelsize=NULL,
+						u=NULL, 
+						pty="s", 
+						srt1=0, 
+						srt2=0, 
+						mar=c(.7,.7,.7,.7), 
+						matrixfill=gray(.7), 
+						pcol1="black", 
+						pcol2=pcol1,     # player colors
+						numcol1=pcol1, 
+						numcol2=pcol2,   # payoff colors
+						scol1=pcol1, 
+						scol2=pcol2,     #strategy colors
+						gameborder=TRUE, 
+						bordercol="white", 
+						borderdim = NULL, 	
+						lwd=3,					
+ 						offset1=c(.8,.8), 
+						offset2=c(.2,.2), 
+						arrowdist=.2, 
+						radius=.2, 
+						space=max(arrowdist, radius+.075), 
+						arrow1=TRUE, 
+						arrow2=arrow1, 
+						arrow1col=gray(.4), 
+						arrow2col=arrow1col, 
+						width1=3, 
+						width2=width1, 
+						nash=TRUE, 	
+						starfill="orange",
+						tips=8, 
+						nashborder="black", 						
+						playersize=NULL, 
+						labelsdim = NULL, 
+						pdim =NULL, 
+						tscale=1, 
+						main="", 
+						tfont="HersheySerif",
+						maincex=1, 
+						col.main="red", 
+						mainline= NA  # Arguments for plot title
+						){ 
+
+	# Checks
+	if ((nrow(X)!=nrow(Y))|(ncol(X)!=ncol(Y))) stop("Dimensions of X and Y are not equal")
+	if (is.null(labels1)) labels1 <- mapply(paste, "X",1:nrow(X), sep="")
+	if (is.null(labels2)) labels2 <- mapply(paste, "Y",1:ncol(X), sep="")
+	if (length(labels1)!=nrow(X)|length(labels2)!=ncol(X)) stop ("Labels do not match options")
+	if (is.character(labels1)==FALSE | is.character(labels2)==FALSE) stop ("Labels are not character strings")
+	
+	# Prepare Scaling (May be modified wth kscale)
+	k <- tscale*2.4*(.85)^(max(length(labels1), length(labels2))-2)
+	if (is.null(u)) u <- tscale*(3 - ((max(nchar(as.character(c(X,Y)))))- 1)*.5 - .25*(max(length(labels1), length(labels2)) - 1))
+	if (is.null(labelsize)) labelsize <- k; if (is.null(playersize)) playersize<- k
+	if (is.null(borderdim)) borderdim <- c(-.3*max(length(labels1), length(labels2)) - .1*labelsize, .25*max(length(labels1), length(labels2)) + .1*labelsize)
+
+	# Start Graph
+	par(pty=pty, mar=mar)
+	plot(1,1, xlim=c(borderdim[1],ncol(X)+.1), ylim=c(-0.1,nrow(X)+borderdim[2]), ann=F, axes=F, asp=par("pin")[1]/par("pin")[2], type="n")
+	title(main=main, cex.main = maincex, col.main=col.main, line = mainline)
+	if(gameborder) polygon(c(borderdim[1],borderdim[1], ncol(X)+.1, ncol(X)+.1, borderdim[1]), c(-.1, nrow(X)+ borderdim[2], nrow(X)+borderdim[2], -.1, -.1), col=bordercol, border=NA)
+	polygon(c(0,ncol(X),ncol(X),0), c(0,0,nrow(X),nrow(X)), lwd=lwd, col=matrixfill) 
+	segments(1:(ncol(X)-1), rep(0,(ncol(X)-1)), 1:(ncol(X)-1), rep(nrow(X),(ncol(X)-1)), lwd=lwd) 
+	segments(rep(0,(ncol(X)-1)), 1:(nrow(X)-1), rep(ncol(X),(ncol(X)-1)), 1:(nrow(X)-1), lwd=lwd)
+
+	a1 <- rep((1-offset1[1]),nrow(X)); for(i in 2:ncol(X)) a1<-c(a1,rep((i-offset1[1]),nrow(X)))
+	b1 <- rep(seq((nrow(X)-offset1[2]), (1-offset1[2]),-1), ncol(X))
+	a2 <- rep((1-offset2[1]),nrow(X)); for(i in 2:ncol(X)) a2<-c(a2,rep((i-offset2[1]),nrow(X)))
+	b2 <- rep(seq((nrow(X)-offset2[2]),(1-offset2[2]),-1), ncol(X))
+		
+	text(a1,b1, as.character(as.vector(X)), cex=u, col=numcol1)
+	text(a2,b2, as.character(as.vector(Y)), cex=u, col=numcol2)
+
+	if (is.null(labelsdim)) labelsdim <- c(-.10*max(length(labels1), length(labels2)), .08*max(length(labels1), length(labels2)))
+#	if (is.null(pdim)) pdim <-c(-.3*max(length(labels1), length(labels2)), .25*max(length(labels1), length(labels2)))
+	if (is.null(pdim)) pdim <-c(-.2*max(length(labels1), length(labels2)), .15*max(length(labels1), length(labels2)))
+	# Action Labels	
+	text(rep(labelsdim[1],nrow(X)), (rep(nrow(X)+.5,nrow(X)) - 1:nrow(X)), labels1, cex=labelsize, family=tfont, font=2, srt=srt1, col=scol1)
+	text(1:ncol(X)-.5, rep(nrow(X)+ labelsdim[2],ncol(X)), labels2, cex=labelsize, family=tfont, font=2, srt=srt2, col=scol2)
+
+	# Player Labels
+	text(pdim[1], nrow(X)/2, P1, cex=playersize, srt=90, family=tfont, font=2, col=pcol1)
+	text(ncol(X)/2, nrow(X)+ pdim[2], P2, cex=playersize, family=tfont, font=2, col=pcol2)
+
+	if (arrow1) gt_BRarrow(X,Y,space=space, nash=nash, color=arrow1col, width=width1, arrowdist=arrowdist)
+	if (arrow2) gt_BRarrow(X,Y,space=space, nash=nash, color=arrow2col, width=width2,  arrowdist=arrowdist, vert=TRUE)
+
+	pureNEx <- array(NA, c(nrow(X), ncol(X)))
+	pureNEy = pureNEx
+	for (i in 1:nrow(X)){
+	for (j in 1:ncol(X)){	
+
+	pureNEx[i,j] <- ifelse((X[i,j]==max(X[,j]) & (Y[i,j]==max(Y[i,]))),j-.5, NA)
+	pureNEy[i,j] <- ifelse((X[i,j]==max(X[,j]) & (Y[i,j]==max(Y[i,]))),nrow(X)-i+.5, NA)
+	}}	
+	if (nash) gt_star(as.vector(pureNEx), as.vector(pureNEy), rad=radius, phi=0, starfill=starfill, tips=tips)
+	}
+	
+
+# Example of asymmetric matrix
+#M1 = matrix(1:12, 3, 4)
+#gt_bimatrix(M1, M1, labels1 =paste(1:3), labels2 = paste(1:4), main = "Asymmetric", mainline = -1
+#            )
---
+++ hop/R/gt_helpers.R
@@ -0,0 +1,90 @@
+# Helper files
+
+
+#' Helper to shift coordinates
+#'
+#' Generate a sequence centered on s, with length l, and gaps of d. Used to align multiple arrows.
+#' @param 
+#' @keywords 
+#' @export
+#' @examples
+#' gt_shift()
+
+gt_shift <- function(s, # center
+                      l, # length  
+                      d  # size of step
+                      ) {
+              sort(s + d * rep(0:l, each = 2)[2:(l + 1)] * (-1)^(1:l), decreasing = TRUE)
+            }
+
+
+#' Helper to make best response arrows for bimatrix
+#'
+#' A function to create best response arrows for the matrix 
+#' @param 
+#' @keywords 
+#' @export
+#' @examples
+#' gt_BRarrow()
+
+gt_BRarrow <- function(
+  X,
+  Y, 
+  vert=FALSE, 
+  arrowdist=.2, 
+  space=.15,
+  color=gray(.3), 
+  nash=TRUE, 
+  width=3
+  ){
+  d <- array(arrowdist, c(nrow(X),ncol(X))); numarrow1 <- rep(NA, nrow(Y)); numarrow2 <- rep(NA, ncol(Y)) ##summarizing arrow info
+  for (i in 1:nrow(Y)){
+    for (j in 1:ncol(Y)){ 
+      if (nash) d[i,j] <- ifelse((X[i,j]==max(X[,j])) & (Y[i,j]==max(Y[i,])),space, arrowdist) 
+      numarrow1[i] <- sum(Y[i,j]==max(Y[i,]))*(ncol(Y)-sum(Y[i,j]==max(Y[i,])))
+      numarrow2[j] <- sum(X[i,j]==max(X[,j]))*(nrow(Y)-sum(X[i,j]==max(X[,j])))
+    }}
+  num <- max(c(numarrow1, numarrow2,1)) #(1 included to avoid 0 denominator)
+  if(vert) {Y <- t(X)}
+  for (i in 1:nrow(Y)){
+    A <- array(NA, c(ncol(Y), ncol(Y))); B <- array(NA, c(ncol(Y), ncol(Y)))		
+    for (j in 1:ncol(Y)){
+      for (k in 1:ncol(Y)){ 
+        if((Y[i,j]==max(Y[i,])) & (Y[i,j]>Y[i,k])){  
+          if (!vert)	 A[j,k] <- k-.5-d[i,k]*(k>j)+d[i,k]*(k<j);  if(vert) A[j,k] <- ncol(Y)-k + .5 +d[k,i]*(k>j)-d[k,i]*(k<j)
+          if (!vert)	 B[j,k] <- j-.5-d[i,j]*(j>k)+d[i,j]*(j<k); if(vert) B[j,k] <- ncol(Y)-j + .5 -d[j,i]*(k>j)+d[j,i]*(k<j)
+        }}}	
+    r <- sum(!is.na(A))  ## number of arrows
+    if (r!=0) {
+      if (!vert) arrows((na.omit(as.vector(A))),gt_shift((nrow(X)-i+.5),r, .3/r) - .15/r*(r%%2==0), na.omit(as.vector(B)),gt_shift((nrow(X)-i+.5),r, .3/r) - .15/r*(r%%2==0), lwd=width, col=color, angle=35/num) 
+      if (vert) arrows(as.vector(gt_shift((i-.5),r, .3/r))- .15/r*(r%%2==0), na.omit(as.vector(A)), as.vector(gt_shift((i-.5),r, .3/r)) - .15/r*(r%%2==0), na.omit(as.vector(B)), lwd=width, col=color, angle=35/num)}
+  }
+ }
+
+
+#' Helper to draw stars
+#'
+#' A function to draw stars 
+#' @param 
+#' @keywords 
+#' @export
+#' @examples
+#' gt_star()
+
+# A function to draw a star
+gt_star <- function(
+  x,                     # x coordinate
+  y,                     # y coordinate
+  rad=1,                 # radius   
+  phi=0,                 # rotation of stars (counter-clockwise, radians),
+  starfill="turquoise",  # color inside star
+  outer="black",         # color of border
+  tips=8                 # tips
+  ){ 
+  if (length(x)!=length(y)) stop("Vector lengths of x and y are different") 
+  for (i in 1:length(x)){
+      polygon(rad*c(rep(c(1,.5),tips),1)*cos(seq(0,2,1/tips)*pi + pi/2 + phi)+x[i], 
+              rad*c(rep(c(1,.5),tips),1)*sin(seq(0,2,1/tips)*pi + pi/2 +phi) + y[i], 
+              col=starfill, border=outer, lwd=3)
+    }
+  }	
